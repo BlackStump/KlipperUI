d26966e0e83aa8b047945ff611924eda5c73df6c
BlackStump <altocoey@hotmail.com>
2025-10-04 13:36:42 +1000

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -51,7 +51,7 @@ main_menu() {
                 change_ui
                 ;;
             5)
-                can-install
+                can_install
                 ;;
             6)
                 echo "Exiting script."
