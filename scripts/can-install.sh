e5bc41e5ec8fe44abc202ed1233084a0bb6aa6dc
BlackStump <altocoey@hotmail.com>
2025-10-04 13:00:52 +1000

    Update can-install.sh

--- scripts/can-install.sh
+++ scripts/can-install.sh
@@ -18,3 +18,5 @@ install_can_service() {
     echo -e 'SUBSYSTEM=="net", ACTION=="change|add", KERNEL=="can*"  ATTR{tx_queue_len}="128"' | sudo tee /etc/udev/rules.d/10-can.rules > /dev/null
     echo -e "[Match]\nName=can*\n\n[CAN]\nBitRate=1M\n\n[Link]\nRequiredForOnline=no" | sudo tee /etc/systemd/network/25-can.network > /dev/null
 }
+
+install_can_service
